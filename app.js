7bb7b39b9c127848839833f4ea6c69524301aa3b
Osama Soliman <prof.o.soliman@gmail.com>
2020-08-07 22:30:10 +0200

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+var robot = require("robotjs");
+
+setInterval(()=>{
+    robot.keyTap("2");
+},2000)
+setInterval(()=>{
+    robot.keyTap("3");
+},2000)
+setInterval(()=>{
+    robot.keyTap("4");
+},2000)
+setInterval(()=>{
+    robot.keyTap("5");
+},2000)
